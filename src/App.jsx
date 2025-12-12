f68c8df2faa08be0acd3a36c9e2ecdf081818833
Kishor <kishorxetri123@gmail.com>
2025-12-12 12:35:51 +0545

    Hero Section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Hero from './components/Hero.jsx';
 import WhoWeAre from './components/WhoWeAre.jsx';
@@ -17,32 +18,46 @@ import AppointmentSection from "./components/AppointmentSection.jsx";
 import MapLocation from "./components/MapLocation.jsx";
 import Newsletter from "./components/Newsletter.jsx";
 import Footer from "./components/Footer.jsx";
+import ChairmanMessage from "./pages/ChairmanMessage.jsx";
+import Blogs from "./pages/Blogs.jsx";
+import ContactUs from "./pages/ContactUs.jsx";
+
+// Home Page Component
+const HomePage = () => (
+  <>
+    <Header />
+    <Hero />
+    <ReadyToHelp />
+    <Stats />
+    <Services />
+    <WhoWeAre />
+    <CTASection />
+    <HygieneSection />
+    <FacilitiesGallery />
+    <KeyServices />
+    <Packages />
+    <Specialists />
+    <Updates />
+    <BlogArticleGrid />
+    <ClientLogos />
+    <AppointmentSection />
+    <MapLocation />
+    <Newsletter />
+    <Footer />
+  </>
+);
 
 function App() {
   return (
-    <>
-      <Header />
-      <Hero />
-      <ReadyToHelp />
-      <Stats />
-      <Services />
-      <WhoWeAre />
-      <CTASection />
-      <HygieneSection />
-      <FacilitiesGallery />
-      <KeyServices />
-      <Packages />
-      <Specialists />
-      <Updates />
-      <BlogArticleGrid />
-      <ClientLogos />
-      <AppointmentSection />
-      <MapLocation />
-      <Newsletter />
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/chairman-message" element={<ChairmanMessage />} />
+      <Route path="/blogs" element={<Blogs />} />
+      <Route path="/contact" element={<ContactUs />} />
+    </Routes>
   )
 }
 
 export default App
+
 
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -45,8 +45,19 @@ const ContactUs = () => {
 
       {/* Hero Banner Section */}
       <div className="relative w-full h-[300px] bg-gradient-to-r from-[#be127e] to-[#d4148a] flex items-center justify-center font-['Poppins']">
-        {/* Background Pattern Overlay */}
-        <div className="absolute inset-0 opacity-10">
+        {/* Background Image Overlay */}
+        <div
+          className="absolute inset-0 opacity-10"
+          style={{
+            backgroundImage: `url('https://bhagawatidiagnostic.com/wp-content/uploads/2025/11/Message-from-the-chairman.jpg')`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+          }}
+        ></div>
+
+        {/* Pattern Overlay */}
+        <div className="absolute inset-0 opacity-5">
           <div className="absolute inset-0" style={{
             backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.4'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
           }}></div>
